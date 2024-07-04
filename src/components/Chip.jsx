a8f352bfc17a3f442f0819e28218ec77a09bcdb4
AvikRay1001 <avikray1001@gmail.com>
2024-07-04 11:23:31 +0530

    Done

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -4,6 +4,7 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { frameImg } from '../utils'
 import { useRef } from 'react'
+import { animateWithGsap } from '../utils/animations'
 
 const Chip = () => {
     const videoRef = useRef();
@@ -19,7 +20,15 @@ const Chip = () => {
             duration: 2,
             ease: 'power2.inOut'
         })
-    })
+
+        animateWithGsap('.g_fadeIn',{
+            opacity:1,
+            y:0,
+            duration: 1,
+            ease: 'power2.inOut'
+        })
+
+    },[])
 
   return (
     <section className='common-padding'>
@@ -46,12 +55,43 @@ const Chip = () => {
                         <img src={frameImg} alt='frame' className='bg-transparent relative z-10'/>
                     </div>
                     <div className='hiw-video'>
-                            <video className='pointer-events-none' playsInline preload='none' muted autoPlay ref={videoRef}>
-                                <source src={frameVideo} type='video/mp4' />
-                            </video>
-                        </div>
+                        <video className='pointer-events-none' playsInline preload='none' muted autoPlay ref={videoRef}>
+                            <source src={frameVideo} type='video/mp4' />
+                        </video>
+                    </div>
+                </div>
+
+                <p className='text-gray font-semibold mt-3 text-center'>Honkai: Star Rail</p>
+
+            </div>
+
+            <div className="hiw-text-container">
+                <div className="flex flex-1 justify-center flex-col">
+                  <p className="hiw-text g_fadeIn">
+                    A17 Pro is an entirely new class of iPhone chip that delivers our {' '}
+                    <span className="text-white">
+                      best graphics performance by far.
+                    </span>
+                  </p>
+                    <br/>
+                
+                    <p className="hiw-text g_fadeIn">
+                      Mobile {' '}
+                      <span className="text-white">
+                        games will look and feel so immersive
+                      </span>,
+                      with incredibibly detailed environments and more realistic characters and with industry-leading speed and efficiency, A17 Pro takes fast and runs with it.
+                    </p>
+
+                </div>
+                
+                <div className='flex-1 flex justify-center flex-col g_fadeIn'>
+                    <p className='hiw-text'>New</p>
+                    <p className='hiw-bigtext'>Pro-class GPU</p>
+                    <p className='hiw-text'>with 6 cores</p>
                 </div>
             </div>
+
         </div>
     </section>
   )
